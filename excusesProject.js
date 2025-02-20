49a5791addc8758e948bd3fe67c09a010102e48d
Oscar Bolaños Chacon <totmom07@gmail.com>
2025-02-20 03:39:58 +0000

    Se optimiza el codigo

--- excusesProject.js
+++ excusesProject.js
@@ -1,18 +1,16 @@
 function onload() {
+  function randomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   let who = ["Mi tio", "Mi abuela", "La maestra", "La vecina"];
   let action = ["boto", "perdio", "reviso", "quebro"];
   let what = ["mi cartera", "las llaves", "el examen", "la ventana"];
   let when = ["en la tarde", "ayer", "hace uuu", "la semana pasada"];
 
-  return (
-    who[Math.floor(Math.random() * who.length)] +
-    " " +
-    action[Math.floor(Math.random() * action.length)] +
-    " " +
-    what[Math.floor(Math.random() * what.length)] +
-    " " +
-    when[Math.floor(Math.random() * when.length)]
-  );
+  return `${randomElement(who)} ${randomElement(action)} ${randomElement(
+    what
+  )} ${randomElement(when)}`;
 }
 
 document.getElementById("excuse").innerHTML = onload();
